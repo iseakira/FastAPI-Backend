8a8c1975c0be5be2f86fbaba5ff9f976ce97b703
IseAkira <akira.ise.m@gmail.com>
2026-01-26 22:07:25 +0900

    簡単なデータベースの定義

--- app/main.py
+++ app/main.py
@@ -2,3 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+shipment = {
+   12076: {
+        "weight":2.0,
+        "content":"Books",
+        "status": "in transit"
+   },
+    12077: {
+          "weight":5.5,
+          "content":"Electronics",
+          "status": "delivered"
+    },
+    12078: {
+          "weight":1.2,
+          "content":"Clothes",
+          "status": "pending"
+    },
+    12079: {
+          "weight":3.0,
+          "content":"Toys",
+          "status": "in transit"
+    }
+}
+
