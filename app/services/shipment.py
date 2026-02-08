14bc45684762af4d0ebfbf20f846778dd256431f
IseAkira <akira.ise.m@gmail.com>
2026-02-08 15:17:02 +0900

    shipmentserviceを定義しました

---
+++ app/services/shipment.py
@@ -0,0 +1,41 @@
+from app.api.schemas.shipment import ShipmentCreate, ShipmentUpdate, ShipmentStatus
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.models import Shipment
+from datetime import datetime, timedelta
+
+class ShipmentService:
+  def __init__(self,session:AsyncSession):
+    self.session=session
+
+
+  async def get(self,id:int) -> Shipment:
+    return await self.session.get(Shipment,id)
+
+
+
+  async def add(self,shipment_create:ShipmentCreate) -> Shipment:
+     new_shipment = Shipment(
+        **shipment_create.model_dump(),
+        status = ShipmentStatus.placed,
+        estimated_delivery = datetime.now() + timedelta(days=3)
+    )
+     self.session.add(new_shipment)
+     await self.session.commit()
+     await self.session.refresh(new_shipment)
+     return new_shipment
+
+  async def update(self,id:int,shipment_update:ShipmentUpdate):
+    shipment = await self.session.get(Shipment,id)
+    shipment.sqlmodel_update(
+        shipment_update.model_dump(exclude_none=True)
+    )
+    await self.session.commit()
+    await self.session.refresh(shipment)
+    return shipment
+
+  async def delete(self,id:int):
+    await self.session.delete(await self.session.get(Shipment,id))
+    await self.session.commit()
+
+
