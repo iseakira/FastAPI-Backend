b0c4b03c9cd1211605da59c0abb5076e2a17a185
IseAkira <akira.ise.m@gmail.com>
2026-03-01 05:08:59 +0900

    同期マイグレーションファイルの作成、すでにDBは存在するのでpassは空

--- migrations/env.py
+++ migrations/env.py
@@ -2,12 +2,23 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlmodel import SQLModel
+
+from app.config import db_settings
+from app.database.models import Shipment,Seller
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+sync_url = db_settings.POSTGRES_URL.replace("+asyncpg", "")
+
+config.set_main_option("sqlalchemy.url",sync_url)
+print("Tables:", SQLModel.metadata.tables.keys())
+
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
