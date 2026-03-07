9189fa2517f241d84bd3ee40989ed01b79683ef0
IseAkira <akira.ise.m@gmail.com>
2026-03-07 21:34:58 +0900

    MailSerberの実装、

---
+++ app.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from fastapi_mail import ConnectionConfig,FastMail,MessageSchema, MessageType
+from app.config import notification_settings
+
+fastmail=FastMail(
+  ConnectionConfig(
+    **notification_settings.model_dump()
+  )
+)
+
+async def send_message():
+  await fastmail.send_message(
+  message=MessageSchema(
+    recipients=["6323007@ed.tus.ac.jp"],
+    subject = "Your Email Delivered With FastShip",
+    body = "Things are about to get interesting...",
+    subtype=MessageType.plain
+  )
+
+)
+print("Email sent!")
+
+asyncio.run(send_message())
