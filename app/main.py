c9657756b83626529220bfde9267d26f3c245cd9
IseAkira <akira.ise.m@gmail.com>
2026-04-15 19:49:08 +0900

    cors修正

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-from fastapi import  BackgroundTasks, FastAPI
+from fastapi import  FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from scalar_fastapi import get_scalar_api_reference
 from contextlib import asynccontextmanager
-from app.core.exceptions import FastShipError, InvalidToken
+from app.core.exceptions import FastShipError
 from app.database.session import create_db_tables
 
 from app.api.router import master_router
-from app.services.notification import NotificationService
 
 @asynccontextmanager
 async def lifespan_handler(app:FastAPI):
@@ -16,6 +16,12 @@ async def lifespan_handler(app:FastAPI):
 app = FastAPI(lifespan = lifespan_handler)
 
 app.include_router(master_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"]
+)
 
 
 def error_handler(request,exception:FastShipError):
@@ -27,17 +33,6 @@ def error_handler(request,exception:FastShipError):
 app.add_exception_handler(FastShipError,error_handler)
 
 
-
-@app.get("/mail")
-async def send_test_mail(tasks:BackgroundTasks):
-    tasks.add_task(
-        NotificationService().send_email,
-        recipients=["akira.ise.m@gmail.com"],
-        subject = "Test Mail Coming Through Once",
-        body = "You should not be interesteted in every body"
-    )
-
-    return {"detail":"Sending mail..."}
 
 
 @app.get("/scalar")
